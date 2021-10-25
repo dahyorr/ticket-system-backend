474f50e9b373e6167853767ed4ac0a6f613d2da0
Dayo Adebanjo <dahyor@outlook.com>
2021-10-25 18:07:44 +0100

    Changed STATIC_URL due to clash

--- tickerrApi/settings.py
+++ tickerrApi/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/public/static/'
+STATIC_URL = '/public/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 AUTH_USER_MODEL = 'userAuth.User'
